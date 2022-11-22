16f1dda2385383866bf22436cb86bf51380416b4
IBA20 <ilya@epir.info>
2022-11-23 00:04:56 +0300

    Order and Ingredient models added

--- bakecakeapp/models.py
+++ bakecakeapp/models.py
@@ -1,3 +1,110 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Order(models.Model):
+    class OrderStatus(models.TextChoices):
+        NEW = '10', _('Новый')
+        SCHEDULED = '20', _('Запланирован')
+        PREPARING = '30', _('Приготовление')
+        DELIVERY = '40', _('Доставка'),
+        COMPLETED = '50', _('Исполнен')
+        CANCELLED = '90', _('Отменен')
+
+    class Shape(models.TextChoices):
+        CIRCLE = 'CR', _('Круг')
+        SQUARE = 'SQ', _('Квадрат')
+        RECTANGLE = 'RC', _('Прямоугольник')
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        related_name='users',
+        verbose_name='Клиент',
+    )
+    levels = models.IntegerField(
+        'Уровни',
+        validators=[MinValueValidator(1), MaxValueValidator(3)]
+    )
+    shape = models.CharField(
+        'Форма',
+        max_length=2,
+        choices=Shape.choices,
+        default=Shape.CIRCLE,
+        db_index=True
+    )
+    writing = models.CharField(
+        'Надпись',
+        max_length=50,
+        blank=True,
+        db_index=True
+    )
+    comments = models.TextField('Комментарии', blank=True)
+    delivery_address = models.CharField(
+        'Адрес доставки',
+        max_length=100,
+        blank=True,
+        db_index=True
+    )
+    delivery_time = models.DateTimeField('Время доставки', db_index=True)
+    status = models.CharField(
+        'Статус',
+        max_length=2,
+        choices=OrderStatus.choices,
+        default=OrderStatus.NEW,
+        db_index=True
+    )
+    courier_info = models.TextField('Информация для курьера', blank=True)
+    created_at = models.DateTimeField(default=timezone.now, db_index=True)
+    value = models.DecimalField(
+        'Стоимость',
+        max_digits=8,
+        decimal_places=2,
+        validators=[MinValueValidator(0)]
+    )
+    paid = models.BooleanField(
+        'Оплачен',
+        db_index=True,
+        default=False,
+    )
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def __str__(self):
+        return f'{self.status} {self.delivery_time}, {self.delivery_address}'
+
+
+class Ingredient(models.Model):
+    class Type(models.TextChoices):
+        TOPPING = 'TP', _('Топпинг')
+        BERRIES = 'BR', _('Ягоды')
+        DECOR = 'DC', _('Декор')
+
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name='ingredients',
+        verbose_name='заказ',
+    )
+    name = models.CharField(
+        'Название',
+        max_length=30,
+        db_index=True
+    )
+    type = models.CharField(
+        'Тип',
+        max_length=2,
+        choices=Type.choices,
+        db_index=True
+    )
+    price = models.DecimalField(
+        'Цена',
+        max_digits=8,
+        decimal_places=2,
+        validators=[MinValueValidator(0)]
+    )
