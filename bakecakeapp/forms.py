6cf7d749c43b8da80f32de14269dc8f2eb461c51
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-11-24 13:27:12 +0300

    Add form rendering for ingredients

--- bakecakeapp/forms.py
+++ bakecakeapp/forms.py
@@ -4,18 +4,24 @@ from .models import Ingredient
 
 
 class CreateCakeForm(forms.Form):
-    #levels
-    #shape
+    levels = forms.ModelChoiceField(
+        label='Уровни',
+        queryset=Ingredient.objects.filter(type='LV'),
+        widget=forms.RadioSelect(attrs={'v-model': 'Levels'}))
+    shape = forms.ModelChoiceField(
+        label='Форма',
+        queryset=Ingredient.objects.filter(type='SH'),
+        widget=forms.RadioSelect(attrs={'v-model': 'Form'}))
     topping = forms.ModelChoiceField(
         label='Топпинг',
         queryset=Ingredient.objects.filter(type='TP'),
-        widget=forms.RadioSelect)
+        widget=forms.RadioSelect(attrs={'v-model': 'Topping'}))
     berries = forms.ModelChoiceField(
         label='Ягоды',
         queryset=Ingredient.objects.filter(type='BR'),
-        widget=forms.RadioSelect)
+        widget=forms.RadioSelect(attrs={'v-model': 'Berries'}))
     decor = forms.ModelChoiceField(
         label='Декор',
         queryset=Ingredient.objects.filter(type='DC'),
-        widget=forms.RadioSelect)
+        widget=forms.RadioSelect(attrs={'v-model': 'Decor'}))
     writing = forms.CharField(max_length=255)
