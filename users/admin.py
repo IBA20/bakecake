d2f092e893756aaaaeca9dc4674f3e7821d6a488
pkzrnvch <pkazarnovich@gmail.com>
2022-11-28 21:39:08 +0500

    Enable admin panel for CustomUser model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('phonenumber', 'is_staff', 'is_active',)
+    list_filter = ('phonenumber', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('phonenumber', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phonenumber', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('phonenumber',)
+    ordering = ('phonenumber',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
