8810d0cc62c7951c5da5fc0fbaae890bb182e78b
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-11-28 11:03:48 +0300

    Add csv export action

--- bakecakeapp/admin.py
+++ bakecakeapp/admin.py
@@ -1,5 +1,34 @@
+import csv
+import datetime
+
 from django.contrib import admin
-from .models import Order, Ingredient
+from django.http import HttpResponse
+
+from .models import Ingredient, Order
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="orders.csv"'
+    writer = csv.writer(response, delimiter=';')
+    fields = [field for field in opts.get_fields()
+              if not field.many_to_many and not field.one_to_many]
+    writer.writerow([field.verbose_name for field in fields])
+
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = 'Экспорт в CSV'
 
 
 @admin.register(Order)
@@ -15,6 +44,7 @@ class OrderAdmin(admin.ModelAdmin):
         'delivery_time'
     )
     list_filter = ('status',)
+    actions = [export_to_csv]
 
 
 @admin.register(Ingredient)
